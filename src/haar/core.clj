3288ad4c5ff667cbf91f442277120485ec32fa62
Geoff Shannon <earthlingzephyr@gmail.com>
2013-03-22 14:23:58 -0700

    Add code for a haar-transform of numbers

--- src/haar/core.clj
+++ src/haar/core.clj
@@ -1,6 +1,15 @@
 (ns haar.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def sig [4 6 10 12 8 6 5 5])
+
+(defn haar-transform [signal]
+  (loop [signal signal
+         [trends flucts :as transform] [[] []]]
+    (if (seq signal)
+      (let [sample (take 2 signal)
+            trend (/ (apply + sample) 2)
+            fluct (/ (apply - sample) 2)]
+        (recur (drop 2 signal)
+               [(conj trends trend)
+                (conj flucts fluct)]))
+      transform)))
